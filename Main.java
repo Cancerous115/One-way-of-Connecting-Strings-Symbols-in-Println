75d1357e2d6b28325e150fa377e4361712d4fe89
Cancerous115 <107626947+Cancerous115@users.noreply.github.com>
2023-02-28 19:09:19 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+class Main{
+  public static void main(String[]args){
+    String name="Johnathan Joestar.";
+    String enemy="Dio Brando.";
+    String travel=" join me on this journey ";
+    
+    System.out.println("Hello World,My name is" + name +"I'm on a quest to defeat"+ enemy + "and" + travel + "Adventurer?");
+  }
+}
